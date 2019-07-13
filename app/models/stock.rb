b5bd66ca27b399e66ff5ed7eddec87063a6d8ba4
KaterynaN <kateryna.od.ua@gmail.com>
2019-07-13 23:09:08 +0300

    Add search by symbol(backend)

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,17 @@
 class Stock < ApplicationRecord
 
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
 
+  def self.new_from_lookup(ticker_symbol)
+    begin
+      client = IEX::Api::Client.new(publishable_token: 'pk_598e8f152a2d4cd9aef0fba5f1211324')
+      looked_up_stock = client.quote(ticker_symbol)
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: looked_up_stock.latest_price)
+    rescue Exception => e
+      return nil
+    end
+  end
 
 end
